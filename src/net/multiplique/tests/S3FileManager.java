f1448492436c49cccaf1875fd295ff569e69d620
Javier Botello <javier.botello@gmail.com>
2023-10-17 18:18:57 +0300

    Funcionalidad de copiado de directorios en S3

--- src/net/multiplique/tests/S3FileManager.java
+++ src/net/multiplique/tests/S3FileManager.java
@@ -4,7 +4,9 @@ import com.amazonaws.SdkClientException;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
@@ -13,6 +15,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
+import java.util.List;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,8 +29,7 @@ public class S3FileManager implements FileManager {
     private static final Regions REGION = Regions.US_EAST_1;
 
     private static final Logger logger = Logger.getLogger(S3FileManager.class.getName());
-    
-    
+
     @Override
     public void copy(String sourceKey, String destKey) throws FileNotFoundException {
         AmazonS3 s3 = AmazonS3ClientBuilder.standard()
@@ -37,13 +39,21 @@ public class S3FileManager implements FileManager {
         if (!s3.doesObjectExist(BUCKET_NAME, sourceKey)) {
             throw new FileNotFoundException("Source does not exist.");
         }
-        
-        if(destKey.endsWith("/")){
-            destKey = destKey + sourceKey.substring(sourceKey.lastIndexOf("/") + 1);
+
+        if (sourceKey.endsWith("/")) {
+            ListObjectsV2Result result = s3.listObjectsV2(BUCKET_NAME, sourceKey);
+            List<S3ObjectSummary> objects = result.getObjectSummaries();
+            for (S3ObjectSummary os : objects) {
+                String destinoFileName = destKey + os.getKey().substring(os.getKey().lastIndexOf("/"));
+                s3.copyObject(BUCKET_NAME, BUCKET_NAME, os.getKey(), destinoFileName);
+            }
+        } else {
+            if (destKey.endsWith("/")) {
+                destKey = destKey + sourceKey.substring(sourceKey.lastIndexOf("/") + 1);
+            }
+            // copyObject(BUCKET_SOURCE, BUCKET_DEST, SOURCE_KEY, DEST_KEY)
+            s3.copyObject(BUCKET_NAME, BUCKET_NAME, sourceKey, destKey);
         }
-        
-        // copyObject(BUCKET_SOURCE, BUCKET_DEST, SOURCE_KEY, DEST_KEY)
-        s3.copyObject(BUCKET_NAME, BUCKET_NAME, sourceKey, destKey);
     }
 
     @Override
@@ -98,7 +108,7 @@ public class S3FileManager implements FileManager {
             Logger.getLogger(S3FileManager.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     @Override
     public void writeImage(BufferedImage image, String keyName) {
         AmazonS3 s3 = AmazonS3ClientBuilder.standard()
@@ -106,10 +116,10 @@ public class S3FileManager implements FileManager {
                 .build();
         String formatName = keyName.substring(keyName.lastIndexOf(".") + 1);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        
+
         try {
             ImageIO.write(image, formatName, baos);
-            InputStream inputStream = new ByteArrayInputStream(baos.toByteArray());            
+            InputStream inputStream = new ByteArrayInputStream(baos.toByteArray());
             ObjectMetadata om = s3.getObject(BUCKET_NAME, keyName).getObjectMetadata();
 
             s3.putObject(BUCKET_NAME, keyName, inputStream, om);
@@ -118,9 +128,9 @@ public class S3FileManager implements FileManager {
             Logger.getLogger(S3FileManager.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     @Override
-    public BufferedImage getImage(String keyName){
+    public BufferedImage getImage(String keyName) {
         AmazonS3 s3 = AmazonS3ClientBuilder.standard()
                 .withRegion(REGION)
                 .build();
